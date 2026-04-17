c0ce619ae44a8bdb40a6dc5469d77cdb2fe4b826
Jonathon Chew <jonchew626@hotmail.com>
2026-04-17 18:09:09 +0100

    new: adding standard stats and conditional stats

--- datakit/__init__.py
+++ datakit/__init__.py
@@ -1,20 +1,45 @@
+from itertools import combinations
+
 import pandas as pd
 
-import cli
+import cli, correlation
 
 df = pd.read_csv(str(cli.flag.get_value("--file")))
 
-rows = df.index
+rows = len(df.index)
 columns = df.columns
 
 print(f"Dataset: {str(cli.flag.get_value("--file"))}")
-print(f"Rows: {len(rows)}")
+print(f"Rows: {rows}")
 print(f"Columns: {len(columns)}")
+
+intColumns = []
+
+threshold = cli.flag.get_value("--corr-threshold")
 
 print("━━━ Column Overview ━━━")
 for column in columns:
     print(column, df[column].dtype)
     print("  - Unique:", df[column].nunique())
-    print("  - Missing:", round(100 - ((len(df[column].dropna().index) / len(rows)) * 100 ), 2), "%")
-    if df[column].dtype == int:
-        print("  - Top:", df[column].nlargest(1))
+    missing = round(100 - ((len(df[column].dropna().index) / rows) * 100 ), 2)
+    if missing > 0.0:
+        print("  - Missing:", missing, "%")
+    if df[column].dtype == int or df[column].dtype == float:
+        print("  - Top:", df[column].nlargest(int(cli.flag.get_value("--top"))).values)
+        if df[column].nunique() != rows:
+            minValue = min(df[column])
+            maxValue = min(df[column])
+            print("  - Min: ", minValue)
+            print("  - Max: ", maxValue)
+            print("  - Range: ", maxValue - minValue)
+        
+        intColumns.append(column)
+    
+    print()
+
+if len(intColumns) > 1:
+    combos = combinations(intColumns, 2)
+    for eachCombo in combos:
+        R = correlation.correlation(df[eachCombo[0]], df[eachCombo[1]])
+        if R > float(threshold):
+            print("Signifcant Correlation: ", eachCombo[0], eachCombo[1], R)
