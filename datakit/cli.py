623ef8e6880bedec65d1769726d2a2533c64ad09
Jonathon Chew <jonchew626@hotmail.com>
2026-04-17 18:08:06 +0100

    new: default flags

--- datakit/cli.py
+++ datakit/cli.py
@@ -4,8 +4,8 @@ from pyflags.flag import Flags
 
 flag = Flags()
 flag.add_file(["--file"], "What file do you want to parse", validator=lambda x: ".csv" in x, required=True)
-flag.add(["--top"], "List the top n number of values", int)
-flag.add(["--corr-threshold"], "How strong should the correclation be", float)
+flag.add(["--top"], "List the top n number of values", int, default=5)
+flag.add(["--corr-threshold"], "How strong should the correclation be", float, default=0.7)
 flag.add(["--include"], "What columns to include", str)
 flag.add(["--exclude"], "What columns to exclude", str)
 flag.add(["--format-text"], "What output should it use", str)
