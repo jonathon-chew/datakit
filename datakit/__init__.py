38c06a728c8c3ae7d5a2a1131a3027f241520c22
Jonathon Chew <jonchew626@hotmail.com>
2026-04-18 07:10:33 +0100

    new: conditionally branching based on dtype which functions are run on the data

--- datakit/__init__.py
+++ datakit/__init__.py
@@ -2,7 +2,7 @@ from itertools import combinations
 
 import pandas as pd
 
-import cli, correlation
+import cli, correlation, Top
 
 df = pd.read_csv(str(cli.flag.get_value("--file")))
 
@@ -14,26 +14,49 @@ print(f"Rows: {rows}")
 print(f"Columns: {len(columns)}")
 
 intColumns = []
-
 threshold = cli.flag.get_value("--corr-threshold")
 
 print("━━━ Column Overview ━━━")
 for column in columns:
     print(column, df[column].dtype)
-    print("  - Unique:", df[column].nunique())
-    missing = round(100 - ((len(df[column].dropna().index) / rows) * 100 ), 2)
-    if missing > 0.0:
-        print("  - Missing:", missing, "%")
+    
+    ### Numeric:
     if df[column].dtype == int or df[column].dtype == float:
-        print("  - Top:", df[column].nlargest(int(cli.flag.get_value("--top"))).values)
-        if df[column].nunique() != rows:
-            minValue = min(df[column])
-            maxValue = min(df[column])
-            print("  - Min: ", minValue)
-            print("  - Max: ", maxValue)
-            print("  - Range: ", maxValue - minValue)
+        missing = round(100 - ((len(df[column].dropna().index) / rows) * 100 ), 2)
+        if missing > 0.0:
+            print("  - Missing:", missing, "%")
+        minValue = df[column].min()
+        maxValue = df[column].nlargest(1).values
+        print("  - Min: ", minValue)
+        print("  - Max: ", maxValue[0])
+        print("  - Range: ", maxValue[0] - minValue)
+        print("  - Mean", sum(df[column]) / len(df[column]))
         
         intColumns.append(column)
+        print()
+        continue
+    
+    ### Categorical:
+    # unique count
+    print("  - Unique:", df[column].nunique())
+    
+    if df[column].nunique() != rows:
+        # top values
+        print("  - Top:", ', '.join(list(Top.Top(df[column].to_list(), int(cli.flag.get_value("--top"))))))
+
+        # imbalance detection
+        imbalence = Top.Imbalence(df[column].to_list(), rows)
+        print("  - Imbalence:", imbalence ) if imbalence else ""
+
+
+    ### Structural flags:
+
+    # is identifier?
+
+    # is constant?
+
+    # is sparse?
+
     
     print()
 
