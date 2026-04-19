40e443265eb1a6ae3fa49ea26b43ce9044decab8
Jonathon Chew <jonchew626@hotmail.com>
2026-04-19 17:30:46 +0100

    update: tweaks to match the new implimentation

--- datakit/Top.py
+++ datakit/Top.py
@@ -2,6 +2,11 @@ from typing import Any
 from collections import defaultdict
 
 def Top(a: list[Any], k: int = 1) -> list[str]:
+    """
+    Returns a list of the top values as a str
+    """
+    if len(a) < k:
+        k = len(a)
 
     if k <= 0:
         raise ValueError("Top needs to be a positive integer!")
@@ -14,11 +19,17 @@ def Top(a: list[Any], k: int = 1) -> list[str]:
     
     _list = sorted(hash.items(), key=lambda item: item[1])
 
-    return [ans[0] for ans in _list[0:k]]
+    return [str(ans[0]) for ans in _list[0:k]]
 
 def Imbalence(a: list[Any], number_of_rows: int=0, threshold: float=0.7) -> dict[str, float]:
+    """
+    Returns a dict of the column name and the imbalence value
+    """
     # top_ratio = count(top_value) / total_rows
     
+    if len(a) < number_of_rows:
+        number_of_rows = len(a)
+
     unique: set[str] = set(a)
     hash = {k: 0.0 for k in unique}
     Imbalence = hash.copy()
