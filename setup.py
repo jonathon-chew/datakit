4254ce4b8f526244fcc92a9b45c8cf7d1efe09d0
Jonathon Chew <jonchew626@hotmail.com>
2026-05-02 13:30:50 +0100

    new: add a setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup (
+    name="datakit",
+    version="v1.0.0",
+    description="""
+A fast, CLI-first dataset profiling tool for quickly understanding CSV files.
+"""
+    author="Jonathon Chew",
+    author_email="jonchew626@hotmail.com",
+    url="https://github.com/jonathon-chew/datakit",
+    packages=find_packages(exclude=("tests*")),
+)
