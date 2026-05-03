5012ca50bcee4b907dc58c93626480a8cef7c00d
Jonathon Chew <jonchew626@hotmail.com>
2026-05-03 12:51:57 +0100

    new: add the dir to start from

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup (
     version="v1.0.0",
     description="""
 A fast, CLI-first dataset profiling tool for quickly understanding CSV files.
-"""
+""",
     author="Jonathon Chew",
     author_email="jonchew626@hotmail.com",
     url="https://github.com/jonathon-chew/datakit",
+    package_dir={"": "src"},
     packages=find_packages(exclude=("tests*")),
 )
